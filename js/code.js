60b72facd90d09758d4f7dfb303efef16b26eee8
LuizED <eduardol@fastmail.com>
2022-09-28 01:36:49 -0300

    add createSquare function

---
+++ js/code.js
@@ -0,0 +1,9 @@
+
+
+function createSquare(){
+    const content =  document.querySelector(".content");
+    const div = document.createElement('div');
+    div.classList.add('square');
+    content.appendChild(div);
+}
+
