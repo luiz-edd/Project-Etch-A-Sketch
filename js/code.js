dd6cbb3af1e04894e5e3ae9edbf7c88abbea6a53
LuizED <eduardol@fastmail.com>
2022-09-28 02:06:58 -0300

    add createGrid function

--- js/code.js
+++ js/code.js
@@ -5,5 +5,22 @@ function createSquare(){
     const div = document.createElement('div');
     div.classList.add('square');
     content.appendChild(div);
+    return div;
 }
 
+function createGrid(gridProportion){
+    for(let i = 0;i<gridProportion; i++){
+        let div = createSquare();
+        for(let j = 0;j < gridProportion;j++){
+            createSquareColumn(div);
+        }
+    }
+}
+
+function createSquareColumn(parent){
+    const div = document.createElement('div');
+    div.classList.add('square-column');
+    parent.appendChild(div);
+}
+
+createGrid(5);
